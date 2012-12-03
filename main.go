8f3009fbe495dfaeda9c6d72405c65149f968544
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2012-12-03 04:31:18 +0100

    identify client with the API

--- main.go
+++ main.go
@@ -1,14 +1,72 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	//	"fmt"
 	"github.com/mattn/go-gtk/gtk"
-	// "log"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 
 	"github.com/digibib/mycel-client/windows"
 )
 
+type response struct {
+	Client Client
+}
+
+type Client struct {
+	Id        int
+	Name      string
+	ScreenRes string `json:"screen_resolution"`
+	ShortTime bool
+	Options   options `json:"options_inherited"`
+}
+
+// These fields must be pointers, in case of null value from JSON
+// When dereferencing check for nil pointers
+type options struct {
+	AgeL     *int    `json:"age_limit_lower"`
+	AgeH     *int    `json:"age_limit_higher"`
+	Printer  *string `json:"printeraddr"`
+	Homepage *string
+}
+
+// Identifies the client from the mac-address and return Client struct
+func Identify(MAC string) (client *Client, err error) {
+	url := "http://localhost:9000/api/clients/?mac=" + MAC
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+	r := new(response)
+	err = json.NewDecoder(resp.Body).Decode(r)
+	if err != nil {
+		return nil, err
+	}
+	return &r.Client, nil
+}
+
 func main() {
+	// Get the Mac-address of client
+	eth0, err := ioutil.ReadFile("/sys/class/net/eth0/address")
+	if err != nil {
+		log.Fatal(err)
+	}
+	MAC := strings.TrimSpace(string(eth0))
+
+	client, err := Identify(MAC)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	gtk.Init(nil)
-	windows.Login("petter-samsung")
+	windows.Login(client.Name)
 	gtk.Main()
 }
