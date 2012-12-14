175c5de4a8d5d734041792c27b0bfdfd9a4f822d
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2012-12-14 08:24:33 +0100

    comments cleanup

--- window/shorttime.go
+++ window/shorttime.go
@@ -5,8 +5,8 @@ import (
 	"strconv"
 )
 
-// Login creates a GTK fullscreen window where users can log inn.
-// It returns when a user successfully authenticates.
+// ShortTime creates a GTK fullscreen window for the shorttime clients.
+// No username/password required, only click 'start' button to log in
 func ShortTime(client string, minutes int) (user string) {
 	// Inital window configuration
 	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
@@ -18,15 +18,10 @@ func ShortTime(client string, minutes int) (user string) {
 	// Build GUI
 	frame := gtk.NewFrame("Logg deg på " + client)
 	frame.SetLabelAlign(0.5, 0.5)
-	// infolabel = Gtk::Label.new
-	// s = "Dette er en korttidsmaskin\nMaks #{@time_limit} minutter!"
-	// infolabel.set_markup "<span foreground='red'>#{s}</span>"
-	// infolabel.set_alignment 0.5, 0.5
 	logo := gtk.NewImageFromFile("logo.png")
 	info := gtk.NewLabel("")
 	info.SetMarkup("<span foreground='red'>Dette er en korttidsmaskin\nMaks " +
 		strconv.Itoa(minutes) + " minutter!</span>")
-	//info.SetAlignment(0.5, 0.5)
 	button := gtk.NewButtonWithLabel("\nStart\n")
 
 	vbox := gtk.NewVBox(false, 20)
